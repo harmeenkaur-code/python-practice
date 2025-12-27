a308043fe9acaef5e610258fe73d5bbdc53329f4
harmeenkaur-code <harmeensarao14@gmail.com>
2025-12-27 17:51:49 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,14 @@
+#what are comments in python
+#single line comment
+#this is a single line comment(single line comment is written using the' #' symbol)
+'''
+Docstring for comments
+python does not have a special multiline comment symbol.
+we use triple quotes for multine comments
+technically these are multiline strings,but they are used as comments when not assigned to a variable
+important points
+comments are not executed
+they do not affect the output
+used for documentation
+symbol for comment(#)
+'''
